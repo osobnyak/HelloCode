d0b6d4fc4e649e5d8d3413a19386c035c1d50aa1
Vladimir.S <vibiray2@mail.ru>
2023-01-06 18:20:27 +0400

    add Example 002 Hello User

---
+++ Example002_HelloUser/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("Введите ваше имя ");
+string username = Console.ReadLine();
+Console.WriteLine("Привет, ");
+Console.WriteLine(username);
+
+Console.Write("Введите ваше имя "); это без переноса строки
+string username2 = Console.ReadLine();
+Console.Write("Привет, ");
+Console.Write(username2);
